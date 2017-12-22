4b8739131b56f046cd93ba8bcc8f9f6bcf5475bf
George Shamian <gosha.shamian@yandex.ru>
2017-12-22 22:27:07 +0300

    быстрая сортировка

---
+++ qsort.py
@@ -0,0 +1,8 @@
+def qsort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return qsort(less) + [pivot] + qsort(greater)
